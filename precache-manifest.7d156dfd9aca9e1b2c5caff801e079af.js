77d24ac67fbf610165fc4e1c2a40db23d4c2bf56
unknown <iamtonmoy01@gmail.com>
2020-07-15 21:36:40 +0600

    Updates

---
+++ precache-manifest.7d156dfd9aca9e1b2c5caff801e079af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23777b086d25e734a5e7f34326d5e88b",
+    "url": "/moviedetails/index.html"
+  },
+  {
+    "revision": "4f9d4a67548a4bb593e9",
+    "url": "/moviedetails/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "a3c133d767c047205b82",
+    "url": "/moviedetails/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "4f9d4a67548a4bb593e9",
+    "url": "/moviedetails/static/js/2.f7fd137b.chunk.js"
+  },
+  {
+    "revision": "748d5fd8a7e7493724665d4098123404",
+    "url": "/moviedetails/static/js/2.f7fd137b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3c133d767c047205b82",
+    "url": "/moviedetails/static/js/main.e577afd4.chunk.js"
+  },
+  {
+    "revision": "2bdbfd4b40b7a7d72cdc",
+    "url": "/moviedetails/static/js/runtime-main.54489d18.js"
+  }
+]);
